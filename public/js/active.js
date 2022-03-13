a3186b1684455962f9a90a06126a5ce5e33dcd85
David Nam <knam13@my.bcit.ca>
2022-03-13 00:16:29 -0800

    Edition 2

--- public/js/active.js
+++ public/js/active.js
@@ -1,49 +1,56 @@
 navbar = document.querySelector("#side-nav").querySelectorAll("a");
-
-var dont = false
-navbar.forEach(element => {
-  element.addEventListener("click", function() {
-    dont = true
-    navbar.forEach(nav => nav.classList.remove("active"))
-
-    this.classList.add("active");
-    setTimeout(() => {
-      dont = false
-    }, 700);
-  })
-})
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 700;
+var count = false;
 
 function updatePage() {
   const { scrollTop, scrollHeight } = document.documentElement;
   const scrollPercent = `${(scrollTop / (scrollHeight - window.innerHeight)) * 100}`;
   console.log(scrollPercent);
-  if (!dont) {
-    if (scrollPercent == 0) {
-      navbar.forEach(nav => nav.classList.remove("active"))
-      navbar[0].classList.add("active");
-      console.log("ASDASd");
-    } else if (scrollPercent > 0 && scrollPercent < 33.476422347514095) {
-      navbar.forEach(nav => nav.classList.remove("active"))
-      navbar[1].classList.add("active");
-      console.log("ASDASd");
-    } else if (scrollPercent >= 33.476422347514095 && scrollPercent < 43.27908764736033) {
-      navbar.forEach(nav => nav.classList.remove("active"))
-      navbar[2].classList.add("active");
-      console.log("ASDASd");
-    } else if (scrollPercent >= 43.27908764736033 && scrollPercent < 54.363147104049204) {
-      navbar.forEach(nav => nav.classList.remove("active"))
-      navbar[3].classList.add("active");
-      console.log("ASDASd");
-    } else if (scrollPercent >= 54.363147104049204 && scrollPercent < 91.39543823680164) {
-      navbar.forEach(nav => nav.classList.remove("active"))
-      navbar[4].classList.add("active");
-      console.log("ASDASd");
-    } else if (scrollPercent >= 91.39543823680164 && scrollPercent < 100.01281394156842) {
-      navbar.forEach(nav => nav.classList.remove("active"))
-      navbar[5].classList.add("active");
-      console.log("ASDASd");
-    }
+
+  if (scrollPercent == 0) {
+    navbar.forEach(nav => nav.classList.remove("active"))
+    navbar[0].classList.add("active");
+  } else if (scrollPercent > 0 && scrollPercent < 33.476422347514095) {
+    navbar.forEach(nav => nav.classList.remove("active"))
+    navbar[1].classList.add("active");
+  } else if (scrollPercent >= 33.476422347514095 && scrollPercent < 43.27908764736033) {
+    navbar.forEach(nav => nav.classList.remove("active"))
+    navbar[2].classList.add("active");
+  } else if (scrollPercent >= 43.27908764736033 && scrollPercent < 54.363147104049204) {
+    navbar.forEach(nav => nav.classList.remove("active"))
+    navbar[3].classList.add("active");
+  } else if (scrollPercent >= 54.363147104049204 && scrollPercent < 91.39543823680164) {
+    navbar.forEach(nav => nav.classList.remove("active"))
+    navbar[4].classList.add("active");
+  } else if (scrollPercent >= 91.39543823680164 && scrollPercent < 100.01281394156842) {
+    navbar.forEach(nav => nav.classList.remove("active"))
+    navbar[5].classList.add("active");
+  }
+
+  if (scrollPercent >= 43.94812680115274 && !count) {
+    count = true;
+    valueDisplays.forEach((valueDisplay) => {
+      let startValue = 0;
+      let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+      let duration = Math.floor(interval / endValue);
+      let counter = setInterval(function() {
+        if (endValue >= 1000000) {
+          startValue += 4651;
+        } else {
+          startValue += 1;
+        }
+        valueDisplay.textContent = startValue.toLocaleString('en');
+        if (startValue >= endValue) {
+          clearInterval(counter);
+          valueDisplay.textContent = endValue.toLocaleString('en');
+        }
+      }, duration);
+    })
   }
 }
+
+
+
 
 document.addEventListener('scroll', updatePage);
