44b229ac0d47038173a0b5773613dc9bd9b40055
dinujagunawardana <97323936+dinujagunawardana@users.noreply.github.com>
2022-03-12 22:38:14 -0800

    Response ipdate

--- public/js/response.js
+++ public/js/response.js
@@ -1,6 +1,5 @@
 function getBotResponse(input) {
-
-  switch (input) {
+  switch (input.toLowerCase()) {
     case "I need help":
       return "What can I help you with today?";
 
@@ -17,12 +16,13 @@ function getBotResponse(input) {
       return "I am sorry to hear that. Believe me, you are not alone. We love you.";
 
     case "I'm scared I am gonna do something dumb":
-      return "Yes there is! Call 310-6789 to get in touch with a 24/7 support agent";
+      return "Yes there is! Call +1 833-456-4566 to get in touch with a 24/7 support agent";
 
     case "I'm worried about my friend but I don't know what to do":
       return "Make sure you check up on him or her as much as you can! Talk to a parent or a councillor if possible";
     default:
-      return "Please input a different question.";
+      return "Please contact the 24/7 support service at 310-6789 or contact the 24/7 suicide hotline at +1 833-456-4566 <br> If you have anymore questions please feel free to email us below.";
   }
+
 
 }
